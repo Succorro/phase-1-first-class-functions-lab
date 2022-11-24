06f24f69bb68531d34f6aa7fe9d081eea64ee745
Steven Gutierrez <stevogbmv@gmail.com>
2022-11-24 11:35:24 -0800

    commit tree 8d387fe6977a2794d44222d3a0926ae96007ae2b

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solution in this file!
+let array = (['Antonia', 'Nuru', 'Amari', 'Mo']);
+//
+const returnFirstTwoDrivers = function(){
+    return array.slice(0, 2);
+}
+//new array, anonymous function
+const returnLastTwoDrivers = function (){
+    return array.slice (-2, 4)
+}
+// new array, anonymous function (-2) reverses start point to two from the end
+const selectingDrivers = ([returnFirstTwoDrivers,returnLastTwoDrivers])
+// array to select which drivers are chosen
+function createFareMultiplier (num1){
+    return function (num2){
+        return num1 * num2;
+    }
+};
+//higher order function
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+//defines anonymous function that is called to either double or triple
+function selectDifferentDrivers (array,selectingDrivers){
+    array = (['Antonia', 'Nuru', 'Amari', 'Mo']);
+    return selectingDrivers();
+}
